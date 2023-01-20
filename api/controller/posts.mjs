157a3eced555994676f218fd6d38d989ce6f2df1
brian <liar88828@gmail.com>
2023-01-20 16:46:31 +0700

    make status

--- api/controller/posts.mjs
+++ api/controller/posts.mjs
@@ -1,11 +1,21 @@
 import {db} from "../connect.mjs";
+import jwt from "jsonwebtoken";
 
 export const getPost = (req, res) => {
-	const q = `SELECT p.*, name, profile_pic
-             FROM post p
-                      JOIN users u ON u.id = p.userId`;
-	db.query(q, (err, data) => {
-		if (err) return res.status(500).json(err)
-		return res.status(200).json(data)
+	const token = req.cookies.accessToken;
+	if (!token) return res.status(401).json('not login')
+	jwt.verify(token, 'secretkey', (err, userInfo) => {
+		if (err) return res.status(403).json('token is not valid')
+		const q = `SELECT p.*, name, profile_pic
+               FROM post p
+                        JOIN users u ON u.id = p.userId
+                        LEFT JOIN relationships r ON u.id = r.followedUserId
+               WHERE r.followUserId = ?
+                  OR p.userId = ?
+               ORDER BY createAt desc `; // AND r.followUserId =?// hanya untuk only saja
+		db.query(q, [userInfo.id, userInfo.id], (err, data) => {
+			if (err) return res.status(500).json(err)
+			return res.status(200).json(data)
+		})
 	})
 }
